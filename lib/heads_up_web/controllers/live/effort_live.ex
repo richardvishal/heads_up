708b07db468dd64a57dcf30ff0b780751ff8297d
richie <richie@ardhika.com>
2025-05-22 23:14:02 +0530

    socket connection check

--- lib/heads_up_web/controllers/live/effort_live.ex
+++ lib/heads_up_web/controllers/live/effort_live.ex
@@ -2,9 +2,9 @@ defmodule HeadsUpWeb.EffortLive do
   use HeadsUpWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket), do: Process.send_after(self(), :update_responders, 2000)
     socket = assign(socket, responders: 0, minutes_per_responder: 10, page_title: "Effort")
     # IO.inspect(socket)
-    Process.send_after(self(), :update_responders, 2000)
     IO.inspect(self(), label: "MOUNT")
     {:ok, socket}
   end
