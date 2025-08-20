60a47ad86084b743dfbe0f1c12ebf3793625ee80
richie <richie@ardhika.com>
2025-08-20 20:47:01 +0530

    composing queries

--- lib/heads_up/incidents.ex
+++ lib/heads_up/incidents.ex
@@ -9,11 +9,29 @@ defmodule HeadsUp.Incidents do
 
   def filter_incidents(filter) do
     Incident
-    |> where(status: ^filter["status"])
-    |> where([i], ilike(i.name, ^"%#{filter["q"]}%"))
-    |> order_by(desc: :name)
+    |> with_status(filter["status"])
+    |> search_by(filter["q"])
+    |> sort_by(filter["sort_by"])
     |> Repo.all()
   end
+
+  defp with_status(query, status) when status in ~w(pending resolved canceled) do
+    where(query, status: ^status)
+  end
+
+  defp with_status(query, _), do: query
+
+  defp search_by(query, q) when q in ["", nil], do: query
+
+  defp search_by(query, q) do
+    where(query, [i], ilike(i.name, ^"%#{q}%"))
+  end
+
+  defp sort_by(query, "name"), do: order_by(query, desc: :name)
+  defp sort_by(query, "priority_desc"), do: order_by(query, desc: :priority)
+  defp sort_by(query, "priority_asc"), do: order_by(query, asc: :priority)
+
+  defp sort_by(query, _), do: query
 
   def get_incident!(id), do: Repo.get!(Incident, id)
 
