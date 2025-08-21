ffd8063f048302b7a4926752631f818d967344f6
richie <richie@ardhika.com>
2025-08-21 19:41:55 +0530

    Added js commands

--- lib/heads_up_web/controllers/live/admin_incidents/index.ex
+++ lib/heads_up_web/controllers/live/admin_incidents/index.ex
@@ -14,13 +14,23 @@ defmodule HeadsUpWeb.AdminIncidents.Index do
   def render(assigns) do
     ~H"""
     <div class="admin-index">
+      <.button phx-click={JS.toggle(to: "#joke", in: "fade-in", out: "fade-out")}>
+        Toggle Joke
+      </.button>
+      <div id="joke" class="joke hidden" phx-click={toggle_blur()}>
+        Why shouldn't you trust trees?
+      </div>
       <.header>
         {@page_title}
         <:actions>
           <.link class="button" navigate={~p"/admin/incidents/new"}>New Incident</.link>
         </:actions>
       </.header>
-      <.table id="incidents" rows={@streams.incidents}>
+      <.table
+        id="incidents"
+        rows={@streams.incidents}
+        row_click={fn {_dom_id, incident} -> JS.navigate(~p"/incidents/#{incident}") end}
+      >
         <:col :let={{_dom_id, incident}} label="Name">
           <.link navigate={~p"/incidents/#{incident}"}>
             {incident.name}
@@ -31,9 +41,11 @@ defmodule HeadsUpWeb.AdminIncidents.Index do
         </:col>
         <:action :let={{_dom_id, incident}}>
           <.link navigate={~p"/admin/incidents/#{incident}/edit"}>Edit</.link>
-          <.link phx-click="delete" phx-value-id={incident.id} data-confirm="Are you sure?">
-          <.icon name="hero-trash" class="h-4 w-4" />
-            Delete
+        </:action>
+
+        <:action :let={{dom_id, incident}}>
+          <.link phx-click={delete_and_fade_out(dom_id, incident)} data-confirm="Are you sure?">
+            <.icon name="hero-trash" class="h-4 w-4" /> Delete
           </.link>
         </:action>
       </.table>
@@ -46,5 +58,19 @@ defmodule HeadsUpWeb.AdminIncidents.Index do
     {:ok, _} = Admin.delete_incident(incident)
 
     {:noreply, stream_delete(socket, :incidents, incident)}
+  end
+
+  defp toggle_blur() do
+    JS.toggle_class("blur", to: "#joke")
+    |> JS.add_class("animate-bounce", to: "#joke")
+    |> JS.remove_class("animate-bounce", to: "#joke", time: 1000)
+  end
+
+  defp delete_and_fade_out(dom_id, incident) do
+    JS.push("delete", value: %{id: incident.id})
+    |> JS.add_class(
+      "fade-out",
+      to: "##{dom_id}"
+    )
   end
 end
